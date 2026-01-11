334f16b6ddebce4a46e074d7f5f41e2661c552ee
Jacob-Derington <der20003@byui.edu>
2026-01-10 18:01:48 -0700

    did the letter ranking program.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("\nWhat is your grade? ");
+        string total = Console.ReadLine();
+        int score = int.Parse(total);
+        string letter = "";
+
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+        else if (score >= 80)
+        {
+            letter = "B";
+        }
+        else if (score >= 70)
+        {
+            letter = "C";
+        }
+        else if (score >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (score >= 70)
+        {
+            Console.WriteLine($"Your grade is {letter}. You pass! Good job!\n");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is {letter}. You fail. Sorry.\n");
+        }
+
     }
 }
