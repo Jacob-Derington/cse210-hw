210581b8f2d22f4fc61e98fd5240292257df122b
Jacob-Derington <der20003@byui.edu>
2026-01-12 12:52:55 -0700

    started prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Write("What is the magic number? ");
+        string num = Console.ReadLine();
+
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        
         Console.WriteLine("Hello Prep3 World!");
     }
 }
