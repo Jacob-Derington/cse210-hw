d5d1909acc1a6f2334788ee70483e7fce45d11fc
Jacob-Derington <der20003@byui.edu>
2026-01-14 13:03:12 -0700

    did more sandboxes.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,3 +1,6 @@
+// cd sandbox -> dotnet new console -o Sandbox 2
+// dotnet new consle -o Sandbox --use-program-main
+// dotnet new console -o sandbox/Sandbox4 --use-program-main
 using System;
 
 class Program
