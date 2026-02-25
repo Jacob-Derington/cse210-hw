4ab79b6019bd4aff235a3c909a58aae235d97efa
Jacob-Derington <der20003@byui.edu>
2026-02-25 13:06:14 -0700

    added the animation along with teh parent and children.

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
-    }
+        Console.WriteLine("\nHello Develop04 World!");
+
+        List<string> animationStrings = new List<string>();
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("-");
+        animationStrings.Add("\\");
+        animationStrings.Add("|");
+        animationStrings.Add("/");
+        animationStrings.Add("-");
+        animationStrings.Add("\\");
+
+        Console.Write("\nHow long do you want to go for? ");
+        string seconds = Console.ReadLine();
+        int t = int.Parse(seconds);
+
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(t);
+
+        int i = 0;
+
+        while (DateTime.Now < endTime)
+        {
+            string s = animationStrings[i];
+            Console.Write(s);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+
+            i++;
+
+            if (i >= animationStrings.Count)
+            {
+                i = 0;
+            }
+        }
+
+        Console.WriteLine("\nDone.\n");
+    }   
 }
