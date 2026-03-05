824ad59adb696b67c2b0030cedfe3f58b5534174
Jacob-Derington <der20003@byui.edu>
2026-03-05 16:02:47 -0700

    got te code doing what i want  to see.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.Clear();
+        Console.WriteLine("\nHello Learning05 World!");
+
+        Console.Clear();
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Navy Blue", 2);
+        shapes.Add(s1);
+
+        Rectagle s2 = new Rectagle("Gray", 3, 6);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("White", 2);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"\nThe {color} shape has an area of {Math.Round(area,3)}.");
+        }
+        Console.WriteLine();
     }
 }
